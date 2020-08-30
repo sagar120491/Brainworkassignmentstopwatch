bffba1c82d80943f33605c5c155f61e3684d9c78
sagar.shelke <cag.sagar121@gmail.com>
2020-08-30 19:39:35 +0530

    Initial Commit

---
+++ Brainworkassignment/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainworkassignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var stopWatch = new Stopwatch();
+
+            Boolean flag = true;
+
+           
+
+                Console.WriteLine("Enter option 1 for start and 2 for stop");
+
+
+
+            
+            do {
+
+                  int option = Convert.ToInt32(Console.ReadLine());
+
+                  switch (option)
+                    {
+                       case 1:
+
+                        stopWatch.Start();
+                        break;
+
+
+                      case 2:
+
+                        stopWatch.Stop();
+
+                        flag = false;
+                        break;
+
+                     default:
+
+
+                        // throw new InvalidOperationException();
+
+                        Console.WriteLine("please enter correwct option");
+                        
+                        break;
+
+
+                       // Console.ReadLine();
+
+                    
+                }
+
+
+
+
+            } while (flag == true);
+
+           
+
+           
+
+
+        }
+        
+    }
+}
---
+++ Brainworkassignment/StopWatch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Brainworkassignment
+{
+    public class Stopwatch
+    {
+        private DateTime start;
+        private DateTime stop;
+       
+        public Stopwatch()
+        {
+            start = new DateTime();
+            stop = new DateTime();
+         
+
+
+
+        }
+
+        public void Start() 
+        {
+          
+            
+                start = DateTime.Now;
+             
+
+                Console.WriteLine("start time" + start.Hour + ":" + start.Minute + ":" + start.Second + ":" + start.Millisecond);
+
+            
+        
+        }
+
+        public void Stop()
+        {
+
+    
+
+            
+
+            stop = DateTime.Now;
+            
+            Console.WriteLine("start time" + stop.Hour + ":" + stop.Minute + ":" + stop.Second + ":" + stop.Millisecond);
+            TimeSpan duration = start - stop;
+            Duration(duration);
+
+
+
+        }
+
+
+
+        private void Duration(TimeSpan duration)
+        {
+
+
+            Console.WriteLine("Dureation:" + duration.Seconds + "seconds" + duration.Milliseconds + "milliseconds");
+
+        }
+
+    }
+}
